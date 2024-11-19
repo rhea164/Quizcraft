f06270853571eac33832dee73a0389788560be2f
asym1 <asym17182@gmail.com>
2024-11-19 14:49:58 +0400

    added DB Connection to student.JS

--- public/student.js
+++ public/student.js
@@ -1,17 +1,15 @@
-import { loadQuizzes } from "./QuizManager.js";
+import { getQuizByCode, loadQuizzes } from "./QuizManager.js";
 
 document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
     
     const quizCode = document.getElementById('student-code').value.trim();
-    const quizzes = loadQuizzes();
 
-
+    const quiz = getQuizByCode(quizCode);
     // Check if the quiz code exists in the quizzes object
-    if (quizzes[quizCode]) {
-      // Redirect to Quiz.html with the quiz code as a URL parameter
-      window.location.href = `/Quiz?code=${quizCode}`;
-    } else {
+    if (quiz == {}) {
       alert("Invalid quiz code. Please try again.");
+    } else {
+        
     }
   });
